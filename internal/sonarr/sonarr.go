22227e96dbf4f9ec9840352d89aebb489ad3ef1d
Joey574 <joeysoroka@outlook.com>
2026-06-25 23:38:42 -0700

    implement sonarr handling

--- internal/sonarr/sonarr.go
+++ internal/sonarr/sonarr.go
@@ -1,12 +1,15 @@
 package sonarr
 
 import (
+	"arr-go/v2/internal/fsu"
 	"arr-go/v2/internal/log"
+	"arr-go/v2/internal/qbit"
 	"os"
+	"strings"
 )
 
 const (
-	_RADARR_SRC_PATH   = "sonarr_episodefile_sourcepath"
+	_SONARR_SRC_PATH   = "sonarr_episodefile_sourcepath"
 	_SONARR_DST_PATH   = "sonarr_episodefile_path"
 	_SONARR_DWN_ID     = "sonarr_download_id"
 	_SONARR_EVENT_TYPE = "sonarr_eventtype"
@@ -39,5 +42,34 @@ func HandleEvent() {
 }
 
 func handleDownload() {
+	src, ok := os.LookupEnv(_SONARR_SRC_PATH)
+	if !ok {
+		log.Fatalf("env variable not defined '%s'", _SONARR_SRC_PATH)
+	}
 
+	dst, ok := os.LookupEnv(_SONARR_DST_PATH)
+	if !ok {
+		log.Fatalf("env variable not defined '%s'", _SONARR_DST_PATH)
+	}
+
+	hash, ok := os.LookupEnv(_SONARR_DWN_ID)
+	if !ok {
+		log.Fatalf("env variable not defined '%s'", _SONARR_DWN_ID)
+	}
+	hash = strings.ToLower(hash)
+
+	log.Infof("recieved args: src='%s', dst='%s', id='%s'", src, dst, hash)
+
+	if err := fsu.Symlink(src, dst); err != nil {
+		log.Fatalf("failed to create symlink: %v", err)
+	}
+
+	sid, err := qbit.Login()
+	if err != nil {
+		log.Errorf("failed to login to qbittorrent: %v", err)
+	}
+
+	if err = qbit.Recheck(sid, hash); err != nil {
+		log.Errorf("failed to recheck torrent: %v", err)
+	}
 }
